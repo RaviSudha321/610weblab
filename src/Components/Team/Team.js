a424852af1f4338865980c921f9f113b96ad13cf
Ravi Sudha <ravi321sudha@gmail.com>
2024-06-19 13:49:10 +0530

    I have worked on team section filters of the about us page. Now the filters are working but content is static for now.

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import './team.css';
 
 
 function Team(){
+
+    const [teamCat, setTeamCat] = useState('ceo');
+
+    const handleCatClick = (cat) => {
+        teamCat !== cat && setTeamCat(cat);
+    }
+console.log(teamCat)
     return(
         <section className='team_sec'>
             <div className='container'>
@@ -9,34 +17,62 @@ function Team(){
                 <p className='description'>Talent wins games, but teamwork and intelligence win championships.</p>
                 <div className='team_content'>
                     <ul className='team_filters'>
-                       <li>CEO</li> 
-                       <li>Team</li>
+                       <li data-title="ceo" onClick={()=>{handleCatClick('ceo')}} className={teamCat == 'ceo' && 'active'}>CEO</li> 
+                       <li data-title="team" onClick={()=>{handleCatClick('team')}} className={teamCat == 'team' && 'active'}>Team</li>
                     </ul>
                     <div className='team_members'>
-                        <div className='member_box'>
+                        <div className={`member_box ${teamCat == 'ceo' ? 'active' : 'hidden'}`} data-cat="ceo">
                             <div className='member_img'>
                                 <img src="images/sushant-sharma.webp" alt="image" />
                             </div>
                             <h3 className='member_name'>Sushant Sharma</h3>
                             <div className='member_designation'>MD</div>
                         </div>
-                        <div className='member_box'>
+                        <div className={`member_box ${teamCat == 'ceo' ? 'active' : 'hidden'}`} data-cat="ceo">
                             <div className='member_img'>
                                 <img src="images/sushant-sharma.webp" alt="image" />
                             </div>
                             <h3 className='member_name'>Sushant Sharma</h3>
                             <div className='member_designation'>MD</div>
                         </div>
-                        <div className='member_box'>
+                        <div className={`member_box ${teamCat == 'ceo' ? 'active' : 'hidden'}`} data-cat="ceo">
                             <div className='member_img'>
                                 <img src="images/sushant-sharma.webp" alt="image" />
                             </div>
                             <h3 className='member_name'>Sushant Sharma</h3>
                             <div className='member_designation'>MD</div>
                         </div>
-                        <div className='member_box'>
+                        <div className={`member_box ${teamCat == 'ceo' ? 'active' : 'hidden'}`} data-cat="ceo">
                             <div className='member_img'>
                                 <img src="images/sushant-sharma.webp" alt="image" />
+                            </div>
+                            <h3 className='member_name'>Sushant Sharma</h3>
+                            <div className='member_designation'>MD</div>
+                        </div>
+                        <div className={`member_box ${teamCat == 'team' ? 'active' : 'hidden'}`} data-cat="team">
+                            <div className='member_img'>
+                                <img src="images/rajan.webp" alt="image" />
+                            </div>
+                            <h3 className='member_name'>Sushant Sharma</h3>
+                            <div className='member_designation'>MD</div>
+                        </div>
+                        <div className={`member_box ${teamCat == 'team' ? 'active' : 'hidden'}`} data-cat="team">
+                            <div className='member_img'>
+                                <img src="images/rajan.webp" alt="image" />
+                            </div>
+                            <h3 className='member_name'>Sushant Sharma</h3>
+                            <div className='member_designation'>MD</div>
+                        </div>
+                        <div className={`member_box ${teamCat == 'team' ? 'active' : 'hidden'}`} data-cat="team">
+                            <div className='member_img'>
+                                <img src="images/rajan.webp" alt="image" />
+                            </div>
+                            <h3 className='member_name'>Sushant Sharma</h3>
+                            <div className='member_designation'>MD</div>
+                        </div>
+                        <div className={`member_box ${teamCat == 'team' ? 'active' : 'hidden'}`} data-cat="team">
+                            <div className='member_img'>
+                                <img src="images/rajan.webp" alt="image" />
                             </div>
                             <h3 className='member_name'>Sushant Sharma</h3>
                             <div className='member_designation'>MD</div>
